ddf68f8768a1dc78a245b03bd2530b5b1ad2be08
Keshav <keshavg80@gmail.com>
2025-08-11 19:24:23 +0530

    fixed overflow not being checked when adjusting offset

--- klib-arena.h
+++ klib-arena.h
@@ -27,31 +27,63 @@ Arena* new_arena();
 void* arena_alloc(Arena* arena, size_t size);
 void arena_reset(Arena* arena);
 void arena_free(Arena* arena);
+ArenaChunk* new_chunk();
 
+
+
+
+// ##########################################################
+//                  Internal Use Functions
+// ##########################################################
+
+ArenaChunk* new_chunk() {
+    ArenaChunk* new = (ArenaChunk*)malloc(sizeof(ArenaChunk));
+    if(!new) {
+        perror("Chunk malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    new->offset = 0;
+    new->next = NULL;
+    new->buffer = (char*)mmap(0, DEFAULT_CHUNK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if(new->buffer == MAP_FAILED) {
+        perror("Arena memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return new;
+}
+
+// ##########################################################
+//                  External Functions
+// ##########################################################
+
+/*
+ * @brief    Creates a new Arena
+ * @param    none
+ * @returns  A pointer to a new Arena
+ */
 Arena* new_arena() {
     Arena* arena = (Arena*)malloc(sizeof(Arena));
-    arena->head = (ArenaChunk*)malloc(sizeof(ArenaChunk));
-    if(!arena || !arena->head) {
+    if(!arena) {
         perror("Arena malloc failed");
         exit(EXIT_FAILURE);
     }
     arena->elems = 0;
 
-    arena->head->offset = 0;
-    arena->head->next = NULL;
-    arena->head->buffer = (char*)mmap(0, DEFAULT_CHUNK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if(arena->head->buffer == MAP_FAILED) {
-        perror("Arena memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
+    arena->head = new_chunk();
 
     return arena;
 }
 
+/*
+ * @brief    Allocates space for an object in the arena
+ * @param    arena - the Arena in which the object is being allocated
+ * @returns  A pointer to the address of the allocated object
+ */
 void* arena_alloc(Arena* arena, size_t size) {
     if(size > DEFAULT_CHUNK_SIZE) {
         fprintf(stderr, "Objects inserted into the arena can have a maximum size of %d bytes\n", DEFAULT_CHUNK_SIZE);
-        exit(EXIT_FAILURE);
+        return NULL;
     }
 
     ArenaChunk* crr = arena->head;
@@ -61,22 +93,8 @@ void* arena_alloc(Arena* arena, size_t size) {
             crr = crr->next;
         }
         else {
-            ArenaChunk* new_chunk = (ArenaChunk*)malloc(sizeof(ArenaChunk));
-            if (!new_chunk) {
-                perror("Chunk malloc failed");
-                exit(EXIT_FAILURE);
-            }
-
-            new_chunk->offset = 0;
-            new_chunk->next = NULL;
-            new_chunk->buffer = (char*)mmap(0, DEFAULT_CHUNK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-            if (new_chunk->buffer == MAP_FAILED) {
-                perror("Chunk mmap failed");
-                exit(EXIT_FAILURE);
-            }
-
-            crr->next = new_chunk;
-            crr = new_chunk;
+            crr->next = new_chunk();
+            crr = crr->next;
         }
 
     }
@@ -87,6 +105,17 @@ void* arena_alloc(Arena* arena, size_t size) {
 
     crr->offset += padding;
 
+    if(crr->offset + size > DEFAULT_CHUNK_SIZE) {
+        if(crr->next != NULL) {
+            crr = crr->next;
+        }
+        else {
+            crr->next = new_chunk();
+            crr = crr->next;
+        }
+        return crr->buffer;
+    }
+    
     void* dest = crr->buffer + crr->offset;
     crr->offset += size;
 
@@ -94,6 +123,11 @@ void* arena_alloc(Arena* arena, size_t size) {
     return dest;
 }
 
+/*
+ * @brief    Resets the arena
+ * @param    arena - the arena being reset
+ * @returns  none
+ */
 void arena_reset(Arena* arena) {
     arena->elems = 0;
     ArenaChunk *current = arena->head;
@@ -105,6 +139,11 @@ void arena_reset(Arena* arena) {
     }
 }
 
+/*
+ * @brief    Frees the arena and consequently all elements within - the arena cannot be used again after this is called
+ * @param    arena - the arena being freed
+ * @returns  A pointer to a new Arena
+ */
 void arena_free(Arena* arena) {
     ArenaChunk *current = arena->head, *next = current->next;
     munmap((void*)current->buffer, DEFAULT_CHUNK_SIZE);
